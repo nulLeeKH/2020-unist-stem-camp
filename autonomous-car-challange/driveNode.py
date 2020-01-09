02cab88010fbe6217b422fd686f6a5dfd65a0a3a
nulLeeKH <i_am@nulleekh.com>
2020-01-09 21:25:34 +0900

    [FEATURE] Add distance specify function and PID controller in autonomous-car-challange/driveNode.py

--- autonomous-car-challange/driveNode.py
+++ autonomous-car-challange/driveNode.py
@@ -8,11 +8,45 @@ from racecar_ws.msg import drive_msg
 from sensor_msgs.msg import Joy
 from sensor_msgs.msg import Image
 from std_msgs.msg import Float32MultiArray
+import time
 
 AUTONOMOUS_MODE=False
 count = 0
 
 print("Complete to open Drive Node")
+
+class PIDControl:
+    def __init__(self):
+        self.Kp = 6
+        self.Ki = 0.0003
+        self.Kd = 0.3
+
+        self.p = 0
+        self.i = 0
+        self.d = 0
+
+        self.privErr = 0
+
+        self.cnt = 0
+
+    def PIDCalc(self, error, interval):
+        self.p = error * self.Kp
+        self.i += error * self.Ki * interval
+        self.d = (error-self.privErr) * self.Kd
+        self.privErr = error
+
+        return self.p + self.i + self.d
+
+class driveCalculator():
+    def findLeast(self, value):
+        least = 10
+        for i in range(len(value)):
+            if value[i] != 0:
+                if value[i]<least:
+                    least = value[i]
+        if least == 10:
+            least = 0
+        return least
 
 class Drive:
     def __init__(self):
@@ -24,8 +58,6 @@ class Drive:
 
         self.laser_sub = rospy.Subscriber("/scan", LaserScan, self.scan_callback)
         self.drive_pub = rospy.Publisher("/drive", drive_msg, queue_size=1)
-        self.cmd.velocity = 100
-        self.cmd.drive_angle = 100
 
         self.cartPoints = [None for x in range(500)]
         self.finalVector = [100, 0]
@@ -44,11 +76,32 @@ class Drive:
 
     def drive_callback(self):
         '''Publishes drive commands'''
+        time.sleep(0.1)
+        left = (drvCalc.findLeast(self.data[49:200]) - 0.1) * 100
+        right = (drvCalc.findLeast(self.data[299:450]) - 0.1) * 100
+
+        front = (drvCalc.findLeast(self.data[0:50] + self.data[450:500]) - 0.18) * 100
+
+        self.cmd.velocity = 255
+
+        PIDAngle = PID.PIDCalc(left-right, 0.0001)
+
+        print(PIDAngle)
+
+        if PIDAngle > 255:
+            PIDAngle = 255
+        elif PIDAngle < -255:
+            PIDAngle = -255
+
+        self.cmd.drive_angle = PIDAngle
+
         self.drive_pub.publish(self.cmd)
 
 
 if __name__ == "__main__":
     try:
+        drvCalc = driveCalculator()
+        PID = PIDControl()
         node = Drive()
         rospy.spin()
     except rospy.ROSInterruptException:
