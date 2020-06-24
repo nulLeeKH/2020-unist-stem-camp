133ef39333bd41a8aefbda6369f610727c76c754
nulLeeKH <i_am@nulleekh.com>
2020-06-24 19:40:55 +0900

    [DOCUMENT] Update license in code

--- autonomous-car-challange/src/vision-control/driveNode.py
+++ autonomous-car-challange/src/vision-control/driveNode.py
@@ -3,7 +3,7 @@
 """
 MIT License
 
-Copyright (c) 2020 Kyung-ha Lee <i_am@nulleekh.com>
+Copyright (c) 2020 Lee Kyung-ha <i_am@nulleekh.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
